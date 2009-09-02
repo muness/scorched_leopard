6d7776dccf99d68b88d58c1d0a53c18aff9659c6
Muness Alrubaie <muness@gmail.com>
2009-09-01 20:04:20 -0400

    fixes...

--- app_installers.sh
+++ app_installers.sh
@@ -1,11 +1,13 @@
 
 function install_iterm {
+  sudo rm -rf /Applications/iTerm.app &&
 	curl -O -s http://iterm.sourceforge.net/iTerm_0.9.6.20090415.zip &&
-	unzip iTerm*.zip &&
+	unzip -q iTerm*.zip &&
 	sudo mv iTerm.app /Applications
 }
 
 function install_mvim {
+  sudo rm -rf /Applications/MacVim.app &&
   curl -O -s http://macvim.googlecode.com/files/MacVim-snapshot-49.tbz &&
   tar xjf MacVim-snapshot-49.tbz &&
   cd MacVim-snapshot-49 &&
@@ -14,23 +16,24 @@ function install_mvim {
 }
 
 function install_mate {
+  sudo rm -rf /Applications/TextMate.app &&
   curl -O -s http://download-b.macromates.com/TextMate_1.5.9.dmg &&
   hdiutil attach TextMate_1.5.9.dmg &&
   cd /Volumes/TextMate\ 1.5.9/ &&
   sudo cp -R TextMate.app /Applications &&
-  cd /tmp &&
+  cd /tmp && sleep 1 &&
   hdiutil detach /Volumes/TextMate\ 1.5.9/ &&
-  ln -s /Applications/TextMate.app/Contents/Resources/mate /usr/local/bin/mate
+  ln -sf /Applications/TextMate.app/Contents/Resources/mate /usr/local/bin/mate
 }
 
 function install_qsb {
+  sudo rm -rf /Applications/Quick\ Search\ Box.app/ &&
   curl -O -s http://qsb-mac.googlecode.com/files/GoogleQuickSearchBox-2.0.0.1674.Release.dmg &&
   hdiutil attach GoogleQuickSearchBox-2.0.0.1674.Release.dmg &&
   cd /Volumes/GoogleQuickSearchBox-2.0.0.1674.Release &&
   sudo cp -R Quick\ Search\ Box.app /Applications/ &&
-  cd /tmp &&
-  hdiutil detach /Volumes/GoogleQuickSearchBox-2.0.0.1674.Release &&
-  open /Applications/Quick\ Search\ Box.app/
+  cd /tmp && sleep 1 &&
+  hdiutil detach /Volumes/GoogleQuickSearchBox-2.0.0.1674.Release/
 }
 
 function install_macports {
@@ -38,8 +41,8 @@ function install_macports {
   hdiutil attach MacPorts-1.8.0-10.5-Leopard.dmg &&
   cd /Volumes/MacPorts-1.8.0 &&
   sudo installer -pkg MacPorts-1.8.0.pkg -target "/" &&
-  cd /tmp &&
-  hdiutil detach /Volumes/MacPorts-1.8.0/
+  cd /tmp && 
+  hdiutil detach /Volumes/MacPorts-1.8.0/ ; echo "..."
 }
 
 function install_ack {
--- setup.sh
+++ setup.sh
@@ -3,17 +3,21 @@
 mkdir ~/tmp
 mkdir ~/bin
 
+sudo rm -rf /tmp/*
+cp app_installers.sh /tmp
+
 pushd /tmp
 sudo sh -c "
+  cd /tmp
   source app_installers.sh && 
   install_iterm &&
   install_mvim &&
   install_mate &&
   install_qsb &&
-  install_macports &&
   install_ack &&
+  install_macports &&
   export PATH=$PATH:/opt/local/bin &&
-  port install git-core +svn+doc+bash_completion"
+  port install git-core +svn+doc+bash_completion mysql5-server postgresql84-server"
 popd
 
 echo "PATH=$PATH:/opt/local/bin:~/bin" >> ~/.bash_profile
@@ -32,10 +36,10 @@ gem: --no-ri --no-rdoc
 '
 echo "$GEM_RC" > ~/.gemrc
 
-curl -L http://github.com/relevance/etc/tree/master%2Fbash%2Fruby_switcher.sh?raw=true?raw=true > ~/ruby_switcher.sh
-echo "source ~/ruby_switcher.sh" >> ~/.bash_profile
-
-install_ruby_186
+# curl -L http://github.com/relevance/etc/tree/master%2Fbash%2Fruby_switcher.sh?raw=true?raw=true > ~/ruby_switcher.sh
+# echo "source ~/ruby_switcher.sh" >> ~/.bash_profile
+# 
+# install_ruby_186
 
 # ARCHFLAGS="-arch i386" gem install mysql -- \
 #   --with-mysql-dir=/usr/local/mysql --with-mysql-lib=/usr/local/mysql/lib \
