25d3fcbce98b133683af003aa996f3ccdbe58620
Muness Alrubaie <muness@gmail.com>
2009-09-15 23:17:17 -0400

    fix in chrome installer

--- app_installers.sh
+++ app_installers.sh
@@ -81,7 +81,7 @@ function install_firefox {
 }
 
 function install_chrome {
-  sudo rm -rf /Applications/GoogleChrome.app &&
+  sudo rm -rf /Applications/Google\ Chrome.app &&
   curl -O -L -s http://dl.google.com/chrome/mac/dev/GoogleChrome.dmg &&
   hdiutil attach GoogleChrome.dmg &&
   sudo cp -R /Volumes/Google\ Chrome/Google\ Chrome.app /Applications &&
